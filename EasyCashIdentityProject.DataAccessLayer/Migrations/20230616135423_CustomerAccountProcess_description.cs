4c087eb73a125c68e736342bfa2fd428ada12a79
Mehmet KOÇAK <tr.mehmetkocak@gmail.com>
2023-06-16 17:56:58 +0300

    Ders 27 sonu

---
+++ EasyCashIdentityProject.DataAccessLayer/Migrations/20230616135423_CustomerAccountProcess_description.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyCashIdentityProject.DataAccessLayer.Migrations
+{
+    public partial class CustomerAccountProcess_description : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "CustomerAccountsProcesses",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "CustomerAccountsProcesses");
+        }
+    }
+}
---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/SendMoneyController.cs
@@ -0,0 +1,58 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Concrete;
+using EasyCashIdentityProject.DtoLayer.Dtos.CustomerAccountProcessDtos;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class SendMoneyController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ICustomerAccountProcessService _customerAccountProcessService;
+
+        public SendMoneyController(UserManager<AppUser> userManager, ICustomerAccountProcessService customerAccountProcessService)
+        {
+            _userManager = userManager;
+            _customerAccountProcessService = customerAccountProcessService;
+        }
+
+        [HttpGet]
+        public IActionResult Index(string mycurrency)
+        {
+            ViewBag.currency=mycurrency;
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(SendMoneyForCustomerAccountProcessDto sendMoneyForCustomerAccountProcessDto)
+        {
+            var context = new Context();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var receiverAccountNumberID = context.CustomerAccounts.Where(x => x.CustomerAccountNumber ==
+            sendMoneyForCustomerAccountProcessDto.ReceiverAccountNumber).Select(y => y.CustomerAccountID).FirstOrDefault();
+
+            //sendMoneyForCustomerAccountProcessDto.SenderID = user.Id;
+            //sendMoneyForCustomerAccountProcessDto.ProcessDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            //sendMoneyForCustomerAccountProcessDto.ProcessType = "Havale";
+            //sendMoneyForCustomerAccountProcessDto.ReceiverID = receiverAccountNumberID;
+            var senderAccountNumberID = context.CustomerAccounts.Where(x => x.AppUserID == user.Id).Where(y => y.CustomerAccountCurrency
+            == "Türk Lirası").Select(z => z.CustomerAccountID).FirstOrDefault();
+
+
+            var values = new CustomerAccountProcess();
+            values.ProcessDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            values.SenderID = senderAccountNumberID;
+            values.ProcessType = "Havale";
+            values.ReceiverID = receiverAccountNumberID;
+            values.Amount = sendMoneyForCustomerAccountProcessDto.Amount;
+            values.Description = sendMoneyForCustomerAccountProcessDto.Description;
+            _customerAccountProcessService.TInsert(values);
+
+            return RedirectToAction("Index", "Deneme");
+        }
+    }
+}
